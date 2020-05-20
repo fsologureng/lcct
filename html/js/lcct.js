bc830fbc8f025162c99ebe5c84125cebbf0b1851
Felipe Sologuren G <sologuren@estudiohum.cl>
2020-05-20 04:17:57 +0000

    Primera versión incompleta de LCCT.

---
+++ html/js/lcct.js
@@ -0,0 +1,101 @@
+// Viewer OpenSeadragon
+var viewer = OpenSeadragon({
+	id: "foto",
+	prefixUrl: "/js/images/",
+	defaultZoomLevel: 79,
+	viewportMargins: {
+		left: 0,
+		top: 0,
+		right: 0,
+		bottom: 0
+	},
+	tileSources: "/iipsrv/iipsrv.fcgi?DeepZoom=lcct.tif.dzi",
+	overlays: Object.values(overlays),
+	showNavigator: false,
+	navigatorPosition: 'BOTTOM_RIGHT',
+	showNavigationControl: false
+});
+
+// Bind a note
+function bindtooltip(el){
+	var tip = $(el);
+	console.log('tip=',tip);
+	var id = tip.prop('id').replace(/^N(\d+)$/,'P$1','ig');
+	console.log('id='+id);
+	$("#"+id).hover(function(e){
+		var bounds = viewer.viewport.getBounds(); 
+		console.log('bounds=',bounds);
+		var viewerTopLeft = viewer.viewport.viewportToWindowCoordinates(bounds.getTopLeft());
+		console.log('viewerTopLeft=',viewerTopLeft);
+		var viewerBottomRight = viewer.viewport.viewportToWindowCoordinates(bounds.getBottomRight());
+		console.log('viewerBottomRight=',viewerBottomRight);
+		var mousex = e.pageX, //Get X coodrinates
+			mousey = e.pageY, //Get Y coordinates
+			tipWidth = tip.width(), //Find width of tooltip
+			tipHeight = tip.height(); //Find height of tooltip
+
+		if ( mousex < (viewerTopLeft.x + viewerBottomRight.x)/2 ) { // left
+			console.log('left');
+			var tipVisX = mousex + 20;
+		}
+		else { // right
+			console.log('right');
+			var tipVisX = mousex - tipWidth - 60;
+		}
+		var tipVisY = viewerTopLeft.y + (viewerBottomRight.y - viewerTopLeft.y - tipHeight)/2;
+
+		/*
+		if ( tipVisX < 20 ) { //If tooltip exceeds the X coordinate of viewport
+			// mousex = e.pageX - tipWidth - 20;
+			mousex = e.pageX - tipWidth/2;
+		} if ( tipVisY < 20 ) { //If tooltip exceeds the Y coordinate of viewport
+			// mousey = e.pageY - tipHeight - 20;
+			mousey = e.pageY + 20;
+		}
+		*/
+		tip.css({ left: tipVisX, top: tipVisY, position: 'absolute' });
+		tip.show(); //Show tooltip
+	}, function() {
+		tip.hide(); //Hide tooltip
+	});
+};
+
+viewer.addHandler('canvas-scroll', function(event) {
+	// The canvas-click event gives us a position in web coordinates.
+	var webPoint = event.position;
+
+	// Convert that to viewport coordinates, the lingua franca of OpenSeadragon coordinates.
+	var viewportPoint = viewer.viewport.pointFromPixel(webPoint);
+
+	// Convert from viewport coordinates to image coordinates.
+	var imagePoint = viewer.viewport.viewportToImageCoordinates(viewportPoint);
+
+	// Show the results.
+	console.log('web=',webPoint.toString(),'viewport=', viewportPoint.toString(), 'image=',imagePoint.toString());
+});
+viewer.addHandler('open', function(event) {
+	// Bind notes
+	$('.lcct-note').each(function(i,el){
+		bindtooltip(el);
+	});
+	// Starting point
+	var oldBounds = viewer.viewport.getBounds(); 
+	console.log('oldBounds=',oldBounds);
+	var fragment = document.location.hash;
+	console.log('fragment=',fragment);
+	var n_point = Number(fragment.replace(/^\#N(\d+)$/,'$1'));
+	console.log('n_point=',n_point);
+	if ( !Number.isNaN(n_point) && n_point> 0 && n_point < 38 ){
+		console.log('fragment is a valid point');
+		var X = overlays['N'+n_point].x < oldBounds.width ? 0 : overlays['N'+n_point].x > lcct_width - oldBounds.width ?lcct_width - oldBounds.width : overlays['N'+n_point].x-oldBounds.width/2;
+		var newBounds = new OpenSeadragon.Rect(X, oldBounds.y, oldBounds.width, oldBounds.height,0); 
+		console.log('newBounds=',newBounds);
+		viewer.viewport.fitBounds(newBounds, true);
+	}
+	else {
+		var newBounds = new OpenSeadragon.Rect(0, oldBounds.y, oldBounds.width, oldBounds.height,0); 
+		console.log('newBounds=',newBounds);
+		viewer.viewport.fitBounds(newBounds, true);
+	}
+});
+
---
+++ html/js/overlays.js
@@ -0,0 +1,300 @@
+var lcct_width = 241159;
+var lcct_height = 1420;
+var overlays = {
+	'N01': {
+		id: 'P01',
+		className: 'lcct-point',
+		placement: 'TOP_LEFT',
+		checkResize: false,
+		x: 175024.762/lcct_width,
+		y: 709.8/lcct_width
+	},
+	'N02': {
+		id: 'P02',
+		className: 'lcct-point',
+		placement: 'TOP_LEFT',
+		checkResize: false,
+		x: 118421.85/lcct_width,
+		y: 614.799/lcct_width
+	},
+	'N03': {
+		id: 'P03',
+		className: 'lcct-point',
+		placement: 'TOP_LEFT',
+		checkResize: false,
+		x: 3255.791/lcct_width,
+		y: 1050.8/lcct_width
+	},
+	'N04': {
+		id: 'P04',
+		className: 'lcct-point',
+		placement: 'TOP_LEFT',
+		checkResize: false,
+		x: 43848.735/lcct_width,
+		y: 522.8/lcct_width
+	},
+	'N05': {
+		id: 'P05',
+		className: 'lcct-point',
+		placement: 'TOP_LEFT',
+		checkResize: false,
+		x: 34353.823/lcct_width,
+		y: 1086.8/lcct_width
+	},
+	'N06': {
+		id: 'P06',
+		className: 'lcct-point',
+		placement: 'TOP_LEFT',
+		checkResize: false,
+		x: 102400/lcct_width,
+		y: 725/lcct_width
+	},
+	'N07': {
+		id: 'P07',
+		className: 'lcct-point',
+		placement: 'TOP_LEFT',
+		checkResize: false,
+		x: 210440.89/lcct_width,
+		y: 847.8/lcct_width
+	},
+	'N08': {
+		id: 'P08',
+		className: 'lcct-point',
+		placement: 'TOP_LEFT',
+		checkResize: false,
+		x: 233711.769/lcct_width,
+		y: 811.8/lcct_width
+	},
+	'N09': {
+		id: 'P09',
+		className: 'lcct-point',
+		placement: 'TOP_LEFT',
+		checkResize: false,
+		x: 41852.903/lcct_width,
+		y: 776.8/lcct_width
+	},
+	'N10': {
+		id: 'P10',
+		className: 'lcct-point',
+		placement: 'TOP_LEFT',
+		checkResize: false,
+		x: 476.783/lcct_width,
+		y: 709.8/lcct_width
+	},
+	'N11': {
+		id: 'P11',
+		className: 'lcct-point',
+		placement: 'TOP_LEFT',
+		checkResize: false,
+		x: 52712.775/lcct_width,
+		y: 840.799/lcct_width
+	},
+	'N12': {
+		id: 'P12',
+		className: 'lcct-point',
+		placement: 'TOP_LEFT',
+		checkResize: false,
+		x: 205817.872/lcct_width,
+		y: 983.8/lcct_width
+	},
+	'N13': {
+		id: 'P13',
+		className: 'lcct-point',
+		placement: 'TOP_LEFT',
+		checkResize: false,
+		x: 162651.858/lcct_width,
+		y: 420.8/lcct_width
+	},
+	'N14': {
+		id: 'P14',
+		className: 'lcct-point',
+		placement: 'TOP_LEFT',
+		checkResize: false,
+		x: 36397.887/lcct_width,
+		y: 929.799/lcct_width
+	},
+	'N15': {
+		id: 'P15',
+		className: 'lcct-point',
+		placement: 'TOP_LEFT',
+		checkResize: false,
+		x: 48535.901/lcct_width,
+		y: 1188.8/lcct_width
+	},
+	'N16': {
+		id: 'P16',
+		className: 'lcct-point',
+		placement: 'TOP_LEFT',
+		checkResize: false,
+		x: 22832.789/lcct_width,
+		y: 1048.799/lcct_width
+	},
+	'N17': {
+		id: 'P17',
+		className: 'lcct-point',
+		placement: 'TOP_LEFT',
+		checkResize: false,
+		x: 91812.849/lcct_width,
+		y: 838.8/lcct_width
+	},
+	'N18': {
+		id: 'P18',
+		className: 'lcct-point',
+		placement: 'TOP_LEFT',
+		checkResize: false,
+		x: 153301.882/lcct_width,
+		y: 1169.799/lcct_width
+	},
+	'N19': {
+		id: 'P19',
+		className: 'lcct-point',
+		placement: 'TOP_LEFT',
+		checkResize: false,
+		x: 217769.712/lcct_width,
+		y: 712.799/lcct_width
+	},
+	'N20': {
+		id: 'P20',
+		className: 'lcct-point',
+		placement: 'TOP_LEFT',
+		checkResize: false,
+		x: 237652.789/lcct_width,
+		y: 1037.8/lcct_width
+	},
+	'N21': {
+		id: 'P21',
+		className: 'lcct-point',
+		placement: 'TOP_LEFT',
+		checkResize: false,
+		x: 131394.758/lcct_width,
+		y: 935.8/lcct_width
+	},
+	'N22': {
+		id: 'P22',
+		className: 'lcct-point',
+		placement: 'TOP_LEFT',
+		checkResize: false,
+		x: 112951.882/lcct_width,
+		y: 589.8/lcct_width
+	},
+	'N23': {
+		id: 'P23',
+		className: 'lcct-point',
+		placement: 'TOP_LEFT',
+		checkResize: false,
+		x: 64885.758/lcct_width,
+		y: 385.8/lcct_width
+	},
+	'N24': {
+		id: 'P24',
+		className: 'lcct-point',
+		placement: 'TOP_LEFT',
+		checkResize: false,
+		x: 73272.786/lcct_width,
+		y: 972.799/lcct_width
+	},
+	'N25': {
+		id: 'P25',
+		className: 'lcct-point',
+		placement: 'TOP_LEFT',
+		checkResize: false,
+		x: 94350/lcct_width,
+		y: 1002/lcct_width
+	},
+	'N26': {
+		id: 'P26',
+		className: 'lcct-point',
+		placement: 'TOP_LEFT',
+		checkResize: false,
+		x: 19540.776/lcct_width,
+		y: 768.799/lcct_width
+	},
+	'N27': {
+		id: 'P27',
+		className: 'lcct-point',
+		placement: 'TOP_LEFT',
+		checkResize: false,
+		x: 202607.805/lcct_width,
+		y: 798.8/lcct_width
+	},
+	'N28': {
+		id: 'P28',
+		className: 'lcct-point',
+		placement: 'TOP_LEFT',
+		checkResize: false,
+		x: 222563.712/lcct_width,
+		y: 928.799/lcct_width
+	},
+	'N29': {
+		id: 'P29',
+		className: 'lcct-point',
+		placement: 'TOP_LEFT',
+		checkResize: false,
+		x: 141244.897/lcct_width,
+		y: 934.799/lcct_width
+	},
+	'N30': {
+		id: 'P30',
+		className: 'lcct-point',
+		placement: 'TOP_LEFT',
+		checkResize: false,
+		x: 187279.739/lcct_width,
+		y: 696.8/lcct_width
+	},
+	'N31': {
+		id: 'P31',
+		className: 'lcct-point',
+		placement: 'TOP_LEFT',
+		checkResize: false,
+		x: 4767.786/lcct_width,
+		y: 868.8/lcct_width
+	},
+	'N32': {
+		id: 'P32',
+		className: 'lcct-point',
+		placement: 'TOP_LEFT',
+		checkResize: false,
+		x: 128861.865/lcct_width,
+		y: 898.799/lcct_width
+	},
+	'N33': {
+		id: 'P33',
+		className: 'lcct-point',
+		placement: 'TOP_LEFT',
+		checkResize: false,
+		x: 61618.777/lcct_width,
+		y: 1156.8/lcct_width
+	},
+	'N34': {
+		id: 'P34',
+		className: 'lcct-point',
+		placement: 'TOP_LEFT',
+		checkResize: false,
+		x: 171222.89/lcct_width,
+		y: 846.8/lcct_width
+	},
+	'N35': {
+		id: 'P35',
+		className: 'lcct-point',
+		placement: 'TOP_LEFT',
+		checkResize: false,
+		x: 54635.777/lcct_width,
+		y: 1048.799/lcct_width
+	},
+	'N36': {
+		id: 'P36',
+		className: 'lcct-point',
+		placement: 'TOP_LEFT',
+		checkResize: false,
+		x: 181747.792/lcct_width,
+		y: 756.799/lcct_width
+	},
+	'N37': {
+		id: 'P37',
+		className: 'lcct-point',
+		placement: 'TOP_LEFT',
+		checkResize: false,
+		x: 70080.805/lcct_width,
+		y: 577.799/lcct_width
+	}
+};
