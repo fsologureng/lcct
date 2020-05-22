9f1750d8578f2f2a030f1a2482bc54188d85c304
Felipe Sologuren G <sologuren@estudiohum.cl>
2020-05-22 03:20:24 +0000

    Cuarta versión incompleta pero ya funcional de LCCT.

--- html/js/lcct.js
+++ html/js/lcct.js
@@ -2,7 +2,7 @@
 var viewer = OpenSeadragon({
 	id: "foto",
 	prefixUrl: "/js/images/",
-	defaultZoomLevel: 79,
+	minZoomLevel: 78,
 	viewportMargins: {
 		left: 0,
 		top: 0,
@@ -11,6 +11,8 @@ var viewer = OpenSeadragon({
 	},
 	tileSources: "/iipsrv/iipsrv.fcgi?DeepZoom=lcct.tif.dzi",
 	overlays: Object.values(overlays),
+	visibilityRatio: 1.0,
+	constrainDuringPan: true,
 	showNavigator: false,
 	navigatorPosition: 'BOTTOM_RIGHT',
 	showNavigationControl: false
@@ -19,27 +21,27 @@ var viewer = OpenSeadragon({
 // Bind a note
 function bindtooltip(el){
 	var tip = $(el);
-	console.log('tip=',tip);
+	// console.log('tip=',tip);
 	var id = tip.prop('id').replace(/^N(\d+)$/,'P$1','ig');
-	console.log('id='+id);
-	$("#"+id).hover(function(e){
+	// console.log('id='+id);
+	var showTip = function(e){
 		var bounds = viewer.viewport.getBounds(); 
-		console.log('bounds=',bounds);
+		// console.log('bounds=',bounds);
 		var viewerTopLeft = viewer.viewport.viewportToWindowCoordinates(bounds.getTopLeft());
-		console.log('viewerTopLeft=',viewerTopLeft);
+		// console.log('viewerTopLeft=',viewerTopLeft);
 		var viewerBottomRight = viewer.viewport.viewportToWindowCoordinates(bounds.getBottomRight());
-		console.log('viewerBottomRight=',viewerBottomRight);
+		// console.log('viewerBottomRight=',viewerBottomRight);
 		var mousex = e.pageX, //Get X coodrinates
 			mousey = e.pageY, //Get Y coordinates
 			tipWidth = tip.width(), //Find width of tooltip
 			tipHeight = tip.height(); //Find height of tooltip
 
 		if ( mousex < (viewerTopLeft.x + viewerBottomRight.x)/2 ) { // left
-			console.log('left');
+			// console.log('left');
 			var tipVisX = mousex + 20;
 		}
 		else { // right
-			console.log('right');
+			// console.log('right');
 			var tipVisX = mousex - tipWidth - 60;
 		}
 		var tipVisY = viewerTopLeft.y + (viewerBottomRight.y - viewerTopLeft.y - tipHeight)/2;
@@ -55,12 +57,21 @@ function bindtooltip(el){
 		*/
 		tip.css({ left: tipVisX, top: tipVisY, position: 'absolute' });
 		tip.show(); //Show tooltip
-	}, function() {
+	};
+	$("#"+id).hover(showTip, function() {
 		tip.hide(); //Hide tooltip
+	});
+	$("#"+id).on('click', function(e) {
+		if( tip.filter(':visible').length > 0 ){
+			tip.hide(); //Hide tooltip
+		}
+		else {
+			showTip(e); //Show tooltip
+		}
 	});
 };
 
-viewer.addHandler('canvas-scroll', function(event) {
+viewer.addHandler('animate', function(event) {
 	// The canvas-click event gives us a position in web coordinates.
 	var webPoint = event.position;
 
@@ -73,48 +84,81 @@ viewer.addHandler('canvas-scroll', function(event) {
 	// Show the results.
 	console.log('web=',webPoint.toString(),'viewport=', viewportPoint.toString(), 'image=',imagePoint.toString());
 });
+
+// focus on a note
+function goToNote(note){
+	// Starting point
+	var oldBounds = viewer.viewport.getBounds();  //FIXME: posición no depende
+	console.log('oldBounds=',oldBounds);
+	var X = overlays[note].x < oldBounds.width ? 0 : overlays[note].x > lcct_width - oldBounds.width ?lcct_width - oldBounds.width : overlays[note].x-oldBounds.width/2;
+	var newBounds = new OpenSeadragon.Rect(X, oldBounds.y, oldBounds.width, oldBounds.height,0); 
+	console.log('newBounds=',newBounds);
+	viewer.viewport.fitBoundsWithConstraints(newBounds, true);
+}
+
+// inicio del viewer
 viewer.addHandler('open', function(event) {
+	// fit vertically
+	viewer.viewport.fitVertically();
 	// Bind notes
 	$('.lcct-note').each(function(i,el){
 		bindtooltip(el);
 	});
-	// Starting point
-	var oldBounds = viewer.viewport.getBounds(); 
-	console.log('oldBounds=',oldBounds);
+	// Posicionamiento en nota
 	var fragment = document.location.hash;
 	console.log('fragment=',fragment);
-	var n_point = Number(fragment.replace(/^\#N(\d+)$/,'$1'));
-	console.log('n_point=',n_point);
-	if ( !Number.isNaN(n_point) && n_point> 0 && n_point < 38 ){
+	if ( fragment.match(/^\#N\d\d/g) && fragment >= '#N01' && fragment <= '#N37' ){
 		console.log('fragment is a valid point');
-		var X = overlays['N'+n_point].x < oldBounds.width ? 0 : overlays['N'+n_point].x > lcct_width - oldBounds.width ?lcct_width - oldBounds.width : overlays['N'+n_point].x-oldBounds.width/2;
-		var newBounds = new OpenSeadragon.Rect(X, oldBounds.y, oldBounds.width, oldBounds.height,0); 
-		console.log('newBounds=',newBounds);
-		viewer.viewport.fitBounds(newBounds, true);
+		goToNote(fragment.replace(/^\#(N\d\d)$/,'$1'));
 	}
 	else {
+		// Starting point
+		var oldBounds = viewer.viewport.getBounds(); // FIXME: arreglar el zoom para diferentes dispositivos 
+		console.log('oldBounds=',oldBounds);
 		var newBounds = new OpenSeadragon.Rect(0, oldBounds.y, oldBounds.width, oldBounds.height,0); 
 		console.log('newBounds=',newBounds);
-		viewer.viewport.fitBounds(newBounds, true);
+		viewer.viewport.fitBoundsWithConstraints(newBounds, true);
 	}
 });
 
 $(document).ready(function(){
 	// bind menú button
 	$('#button').on('click',function(e){
-		$('#menu').fadeToggle("fast",function(){console.log("menu click")});
+		$('#menu').toggle("fast",function(){console.log("menu click")});
 		return false;
 	});
-	$('#menu a[href="#comenta"]').on('click',function(e){
-		console.log('click comenta');
-		$('#menu').fadeToggle("fast",function(){console.log("esconde menu")});
-		$('#comenta').fadeIn("fast",function(){console.log("activa comenta")});
-		$('#mask').fadeIn("fast",function(){console.log("activa máscara")});
+	// access and exit from each section
+	$('.lightbox').each(function(idx,el){
+		var id = el.id;
+		console.log('id=',id);
+		$('#menu a[href="#'+id+'"]').on('click',function(e){
+			console.log('click ',id);
+			$('#menu').hide("fast",function(){console.log("esconde menu")});
+			$('#'+id).show("fast",function(){console.log("activa ",id)});
+			$('#mask').show("fast",function(){console.log("activa máscara")});
+		});
+		$('#'+id+' nav button').on('click',function(e){
+			console.log('click cierra ',id);
+			$('#mask').hide("fast",function(){console.log("desactiva máscara")});
+			$('#'+id).hide("fast",function(){console.log("desactiva ",id)});
+			document.location.hash='';
+		});
 	});
-	$('#comenta nav button').on('click',function(e){
-		console.log('click cierra comenta');
-		$('#mask').fadeOut("fast",function(){console.log("desactiva máscara")});
-		$('#comenta').fadeOut("fast",function(){console.log("desactiva comenta")});
-		document.location.hash='';
+	// bind notes access
+	$('a.lcct-link').on('click',function(e){
+		console.log('click ',e);
+		var fragment = e.target.hash;
+		console.log('fragment=',fragment);
+		// fit vertically
+		viewer.viewport.fitVertically();
+		goToNote(fragment.replace(/^\#(N\d+)$/,'$1'));
+		$('#mask').hide("fast",function(){console.log("desactiva máscara")});
+		$('#notas').hide("fast",function(){console.log("desactiva notas")});
 	});
+	// Posicionamiento en sección
+	var fragment = document.location.hash;
+	console.log('fragment=',fragment);
+	if ( $(''+fragment+'.lightbox').length > 0 ){
+		$('#menu a[href="'+fragment+'"]').trigger('click');
+	}
 });
