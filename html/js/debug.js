389e1782dfcb29a168003ce7f500e6604127b2ba
Felipe Sologuren G <sologuren@estudiohum.cl>
2020-07-14 03:47:28 +0000

    * Agrega media queries para iOS.  * Extiende script para mostrar tamaños.

--- html/js/debug.js
+++ html/js/debug.js
@@ -1,27 +1,57 @@
 $(document).ready(function(){
 	if(window.matchMedia('(max-device-height: 279px)').matches){
-		$('#size').text('max-device-height: 279px');
+		$('#height').text('max-device-height: 279px');
 	}
 	else if(window.matchMedia('(max-device-height: 320px)').matches){
-		$('#size').text('max-device-height: 320px');
+		$('#height').text('max-device-height: 320px');
 	}
 	else if(window.matchMedia('(max-device-height: 350px)').matches){
-		$('#size').text('max-device-height: 350px');
+		$('#height').text('max-device-height: 350px');
 	}
 	else if(window.matchMedia('(max-device-height: 375px)').matches){
-		$('#size').text('max-device-height: 375px');
+		$('#height').text('max-device-height: 375px');
 	}
 	else if(window.matchMedia('(max-device-height: 411px)').matches){
-		$('#size').text('max-device-height: 411px');
+		$('#height').text('max-device-height: 411px');
+	}
+	else if(window.matchMedia('(max-device-height: 568px)').matches){
+		$('#height').text('max-device-height: 568px');
 	}
 	else if(window.matchMedia('(max-device-height: 720px)').matches){
-		$('#size').text('max-device-height: 720px');
+		$('#height').text('max-device-height: 720px');
 	}
 	else if(window.matchMedia('(max-device-height: 768px)').matches){
-		$('#size').text('max-device-height: 768px');
+		$('#height').text('max-device-height: 768px');
 	}
 	else {
-		$('#size').text('taller then 768px');
+		$('#height').text('higher than 768px');
+	}
+	if(window.matchMedia('(max-device-width: 279px)').matches){
+		$('#width').text('max-device-width: 279px');
+	}
+	else if(window.matchMedia('(max-device-width: 320px)').matches){
+		$('#width').text('max-device-width: 320px');
+	}
+	else if(window.matchMedia('(max-device-width: 350px)').matches){
+		$('#width').text('max-device-width: 350px');
+	}
+	else if(window.matchMedia('(max-device-width: 375px)').matches){
+		$('#width').text('max-device-width: 375px');
+	}
+	else if(window.matchMedia('(max-device-width: 411px)').matches){
+		$('#width').text('max-device-width: 411px');
+	}
+	else if(window.matchMedia('(max-device-width: 568px)').matches){
+		$('#width').text('max-device-width: 568px');
+	}
+	else if(window.matchMedia('(max-device-width: 720px)').matches){
+		$('#width').text('max-device-width: 720px');
+	}
+	else if(window.matchMedia('(max-device-width: 768px)').matches){
+		$('#width').text('max-device-width: 768px');
+	}
+	else {
+		$('#width').text('wider than 768px');
 	}
 });
 
